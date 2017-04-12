5d2e82cf55948de58e96c363c1b90689cef3fcaf
Harshal Solanki <harshal.solancomki46@gmail>
2017-04-13 00:25:48 +0530

    Third commit

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,33 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Harshal Solanki on 12/04/17.
+//  Copyright © 2017 Harshal Solanki. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var thumbimg :UIImageView!
+    
+    @IBOutlet weak var namelabel :UILabel!
+
+    var pokemon : Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configurecell(pokemon : Pokemon){
+
+        self.pokemon = pokemon
+        namelabel.text = self.pokemon.name.capitalized
+        thumbimg.image = UIImage(named : "\(self.pokemon.pokedexId)")
+        
+    }
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,32 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Harshal Solanki on 12/04/17.
+//  Copyright © 2017 Harshal Solanki. All rights reserved.
+//
+
+import Foundation
+class Pokemon{
+    
+    private var _name: String!
+    private var _pokedexId : Int!
+    
+    
+    var name : String{
+        
+        return _name
+    }
+    
+    var pokedexId : Int{
+        
+        return _pokedexId
+    }
+    
+    init(name : String , pokedexId : Int) {
+        
+        
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
